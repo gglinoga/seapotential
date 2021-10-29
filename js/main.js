1b1b8b474aec9441c2441b25e154ddbf929f1cce
gglinoga <gglinoga@gmail.com>
2021-10-28 22:52:18 -0700

    final changes

--- js/main.js
+++ js/main.js
@@ -42,6 +42,8 @@ jQuery(document).ready(function($) {
     })
 
     var owl = $("#owl-testimonials");
+    var owl2 = $("#owl-testimonials2");
+    var owl3 = $("#owl-testimonials3");
 
       owl.owlCarousel({
         
@@ -55,6 +57,33 @@ jQuery(document).ready(function($) {
         itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
         
     });
+
+      owl2.owlCarousel({
+          
+        pagination : true,
+        paginationNumbers: false,
+        autoPlay: 6000, //Set AutoPlay to 3 seconds
+        items : 3, //10 items above 1000px browser width
+        itemsDesktop : [1000,3], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,2], // betweem 900px and 601px
+        itemsTablet: [600,1], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        
+    });
+
+      owl3.owlCarousel({
+            
+        pagination : true,
+        paginationNumbers: false,
+        autoPlay: 6000, //Set AutoPlay to 3 seconds
+        items : 3, //10 items above 1000px browser width
+        itemsDesktop : [1000,3], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,2], // betweem 900px and 601px
+        itemsTablet: [600,1], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        
+    });
+
 
 
 });
